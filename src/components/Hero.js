19d3293cba737d2d66932c53c20a7e0df8c35eff
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2025-02-14 17:28:20 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -174,7 +174,7 @@ function Hero() {
     <Box sx={{ position: 'relative', width: '100%' }}>
       <SearchContainer>
         <SearchField
-          placeholder="Search for Face Mask"
+          placeholder="Search for Service"
           variant="outlined"
           InputProps={{
             endAdornment: (
